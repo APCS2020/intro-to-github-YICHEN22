4cf7da7f769e2bc62b505282b943b7464217c418
陈治均 Ethan <50724503+YICHEN22@users.noreply.github.com>
2019-05-17 10:39:27 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test{
+    //f(x) - f name , x parameter
+    //method1
+    public static void printLine(int num, String str ) {
+        for (int i = 0; i < num; i++){
+            System.out .print(str);// str -. a, a1, string
+        }
+        System.out.println();
+    }
+}
